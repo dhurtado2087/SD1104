bd67610f181d9c3ede0d9d4b243068de2912b8fd
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-06-16 13:07:51 -0500

    First commit.

---
+++ ConsoleApp3_5/ConsoleApp3_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lab 3 5");
+        }
+    }
+}
