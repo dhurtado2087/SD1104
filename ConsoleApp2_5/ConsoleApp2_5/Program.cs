71aeae5103d75336d6564441ba78e5259ceb96f0
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-06-23 09:42:08 -0500

    6/23/2020 9:42

---
+++ ConsoleApp2_5/ConsoleApp2_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsoleApp5_2/ConsoleApp5_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5_2
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson 5 2 Lab");
+
+            List<string> taskList = new List<string>();
+
+            foreach (WeekDays days in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for day " + days);
+                string task = days + ":" + Console.ReadLine();
+                taskList.Add(task);
+            }
+            foreach (string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
