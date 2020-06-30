5f6587ac3a367674af08b54413f8b965bf6fe511
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-06-30 16:43:20 -0500

    6/30/2020 4:43

---
+++ ConsoleApp7_4/ConsoleApp7_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp7_4
+{
+    class DatingProfile //1.1 Class
+    {
+        public string firstName; //1.2 Properties
+        private string lastName;
+        public int age;
+        public string gender;
+        public string bio;
+        public DatingProfile(string inputFirstName, string inputLastName, int inputAge, string inputGender, string inputBio) //1.3 Constructor
+        {
+            this.firstName = inputFirstName;
+            this.lastName = inputLastName;
+            this.age = inputAge;
+            this.gender = inputGender;
+            this.bio = inputBio;
+        }
+        public void WriteBio(string text)
+        {
+            Console.WriteLine("What do you want your bio to say?");
+            string userInput = Console.ReadLine();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 7.4: The Dating Game");
+
+
+        }
+    }
+}
