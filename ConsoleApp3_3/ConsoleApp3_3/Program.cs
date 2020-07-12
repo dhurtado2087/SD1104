a6b8e3c61954337ec3ea163ed14b9bebbaf12ac2
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-07-12 17:38:02 -0500

    7/12/2020 5:37

---
+++ ConsoleApp3_3/ConsoleApp3_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson 3.3: Regular Expressions");
+
+            //string textToSearch = "This is Sparta!";
+            //string regex = "[a-z]";
+
+            string textToSearch = "afgkk24679ggZXVFcatcat9970b02dogMaryxxxxbbbb22FHDFISHMOTHERfaskldjflasjfjasdfjzxcvxcEYERTR345262372546vbcvb0000000carTT2356236623732623462366johnxcxcxvbdfsdgvxcvC#00002499xbvvcfgooglecvcvxzvdfgsdjiiruewortuiwerioptunvxn990348590234523907854034sdnskfshwateririweotowyuwerituwreutpwetuwertueryuwertwerutpweurtpuweptnvmxcvnnxcxcxvb";
+            string regex = "[a-z]at";
+
+            MatchCollection matchResults = Regex.Matches(textToSearch, regex);
+
+            foreach(Match m in matchResults)
+            {
+                Console.WriteLine(m);
+            }
+
+
+        }
+    }
+}
