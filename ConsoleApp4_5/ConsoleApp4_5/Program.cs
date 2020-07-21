fe47bb992064f241ff1d23a24f15e32f6b652198
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-07-21 17:09:30 -0500

    07/21/2020 5:09

---
+++ ConsoleApp4_5/ConsoleApp4_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4.5: The Trivia Website Leaderboard");
+
+            List<int> scores = new List<int>();
+            scores.Add(4400);
+            scores.Add(8900);
+            scores.Add(1200);
+            scores.Add(3700);
+            scores.Add(7200);
+
+            Console.WriteLine("Original list");
+            foreach (var item in scores)
+            {
+                Console.WriteLine(item);
+            }
+
+            scores.Sort();
+            Console.Write("List in descending order");
+            foreach (var item in scores)
+            {
+                Console.WriteLine(item);
+            }
+
+            scores.Reverse();
+            Console.WriteLine("List in ascending order");
+            foreach (var item in scores)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
