2eb91303a41bf6056e9166b9e0a789d8e9278835
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-07-21 14:23:37 -0500

    07/21/2020 2:23

---
+++ ConsoleApp11_5/ConsoleApp11_5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp11_5
+{
+    class Actor
+    {
+        [Key]
+        public int actor_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime last_update { get; set; }
+
+        public Actor(string first_name, string last_name)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.last_update = DateTime.Now;
+        }
+    }
+    class MyData
+    {
+        public string name { get; private set; }
+        public int age { get; private set; }
+        public string favoriteColor { get; private set; }
+        public bool hasPet { get; private set; }
+        public string favoriteSport { get; private set; }
+
+
+        public MyData(string name, int age, string favoriteColor, bool hasPet, string favoriteSport)
+        {
+            this.name = name;
+            this.age = age;
+            this.favoriteColor = favoriteColor;
+            this.hasPet = hasPet;
+            this.favoriteSport = favoriteSport;
+        }
+    }
+    class SakilaContext : DbContext
+    {
+        public DbSet<Actor> Actor { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;");
+        }
+    }
+    class Film
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public int  release_year { get; set; } //this may need to be a string
+        public byte language_id { get; set; }
+        public byte original_language_id { get; set; }
+        public byte rental_duration { get; set; }
+        public decimal rental_rate { get; set; }
+        public Int16 length { get; set; }
+        public decimal replacement_cost { get; set; }
+        public string rating { get; set; }
+        public string special_features{ get; set; }
+        public DateTime last_update { get; set; }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 11_5: Database to C# to HTLM");
+
+
+        }
+    }
+}
---
+++ ConsoleApp4_4/ConsoleApp4_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4.4: Improved Spam Checker");
+
+            List<string> blackListWord = new List<string>();
+            blackListWord.Add("buy");
+            blackListWord.Add("viagra");
+            blackListWord.Add("xxx");
+            blackListWord.Add("free money");
+            blackListWord.Add("lifetime offer");
+            blackListWord.Add("send money");
+            blackListWord.Add("bank account");
+            blackListWord.Add("nigeria");
+            blackListWord.Add("online");
+            blackListWord.Add("pharmacy");
+            blackListWord.Add("h8te");
+            blackListWord.Add("meet girls");
+            
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            message = message.ToLower();
+
+            for (int i = 0; i < blackListWord.Count; i++)
+            {
+                if (message.Contains(blackListWord[i]))
+                {
+                    isSpam = true;
+                }
+            }
+            if (isSpam == true)
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+        }
+    }
+}
