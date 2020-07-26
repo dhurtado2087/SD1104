22b4482d62cefcb3a41cdb968283ee71334771e4
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-07-25 20:30:37 -0500

    07/25/2020 8:30

---
+++ ConsoleApp4_6/ConsoleApp4_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4.6: Array To Do List");
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] tasks = new string[7];
+
+            Console.WriteLine("Welcome to your daily planner.");
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine("Type in the new task for " + days[i]);
+                tasks[i] = days[i] + ": " + Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Console.WriteLine(tasks[i]);
+            }
+        }
+    }
+}
